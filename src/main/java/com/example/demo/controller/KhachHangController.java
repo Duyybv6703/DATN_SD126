1ba0a09652f42809201256d4e66c04435281d1f1
Duybv6703 <duybv6703@gmail.com>
2024-11-13 22:24:24 +0700

    update khachhang

--- src/main/java/com/example/demo/controller/KhachHangController.java
+++ src/main/java/com/example/demo/controller/KhachHangController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 
 @Controller
 public class KhachHangController {
@@ -80,6 +82,51 @@ public class KhachHangController {
         khachHangService.add(khachHang);
 
         return "redirect:/khach-hang";
+    }
+
+    @GetMapping("/khachhang/view-update/{id}")
+    public String viewUpdate(@PathVariable("id") String id, Model model) {
+
+        KhachHang khachHang = khachHangService.getOne(id);
+
+        model.addAttribute("detail", khachHang);
+        return "khachhang-update";
+    }
+
+    @PostMapping("/khach-hang/update")
+    public String update(
+            @RequestParam("ma") String ma1,
+            @RequestParam("hoten") String hoten1,
+            @RequestParam("sdt") String sdt1,
+            @RequestParam("email") String email1,
+            @RequestParam("gioitinh") String gioitinh1,
+            @RequestParam("ngaysinh") String ngaysinh1,
+            @RequestParam("taikhoan") String taikhoan1,
+            @RequestParam("matkhau") String matkhau1,
+            @RequestParam("ngaytao") String ngaytao1,
+            @RequestParam("ngaycapnhat") String ngaycapnhat1,
+            @RequestParam("tinhtrang") String tinhtrang1,
+            @RequestParam("id") String id1
+
+    ) {
+        KhachHang khachHang = KhachHang.builder()
+                .ma(ma1)
+                .ho_ten(hoten1)
+                .sdt(sdt1)
+                .email(email1)
+                .gioi_tinh(Boolean.valueOf(gioitinh1))
+                .ngay_sinh(ngaysinh1)
+                .tai_khoan(taikhoan1)
+                .mat_khau(matkhau1)
+                .ngay_tao(ngaytao1)
+                .ngay_cap_nhat(ngaycapnhat1)
+                .tinh_trang(Boolean.valueOf(tinhtrang1))
+                .build();
+
+        khachHangService.update(id1, khachHang);
+
+        return "redirect:/khach-hang";
+
     }
 
 
