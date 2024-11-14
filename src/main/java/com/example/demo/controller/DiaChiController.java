b3a964f5a9d56950efd3562b4f28f53838869a67
Duybv6703 <duybv6703@gmail.com>
2024-11-14 22:39:04 +0700

    detail controller diachi

--- src/main/java/com/example/demo/controller/DiaChiController.java
+++ src/main/java/com/example/demo/controller/DiaChiController.java
@@ -2,12 +2,14 @@ package com.example.demo.controller;
 
 
 import com.example.demo.entity.DiaChi;
+import com.example.demo.entity.KhachHang;
 import com.example.demo.service.DiaChiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -25,6 +27,20 @@ public class DiaChiController {
 
         return "diachi";
 
+    }
+
+    @GetMapping("/dia-chi/detail/{id}")
+    public String detail(@PathVariable("id") String id, Model model) {
+
+        DiaChi diaChi = diaChiService.getOne(id);
+
+        Page<DiaChi> diaChis = diaChiService.getData(0);
+
+        model.addAttribute("hienthi", diaChis);
+
+        model.addAttribute("detail", diaChi);
+
+        return "diachi";
     }
 
 
