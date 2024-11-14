8e015a79674c2d424e0d6ec6071af5739c4904a2
Duybv6703 <duybv6703@gmail.com>
2024-11-14 22:07:51 +0700

    create entity diachi

---
+++ src/main/java/com/example/demo/entity/DiaChi.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "dia_chi")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class DiaChi {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "ma")
+    private String ma;
+
+    @Column(name = "phuong_xa")
+    private String phuongXa;
+
+    @Column(name = "quan_huyen")
+    private String quanHuyen;
+
+    @Column(name = "thanh_pho")
+    private String thanhPho;
+
+    @Column(name = "ngay_tao")
+    private Date ngayTao;
+
+    @Column(name = "ngay_cap_nhat")
+    private Date ngayCapNhat;
+
+    @Column(name = "tinh_trang")
+    private Boolean tinhTrang;
+
+    @Column(name = "mo_ta")
+    private String moTa;
+
+
+}
