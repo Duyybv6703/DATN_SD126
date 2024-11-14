56fd4445c9b8b6f09661d38e237b3ee0bab2d287
Duybv6703 <duybv6703@gmail.com>
2024-11-14 22:25:19 +0700

    create controller load data and pageable diachi

--- src/main/java/com/example/demo/controller/DiaChiController.java
+++ src/main/java/com/example/demo/controller/DiaChiController.java
@@ -1,4 +1,31 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.entity.DiaChi;
+import com.example.demo.service.DiaChiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class DiaChiController {
+
+    @Autowired
+    private DiaChiService diaChiService;
+
+    @GetMapping("/dia-chi")
+    public String hienThiDiaChi(@RequestParam(defaultValue = "0", name = "page") Integer page, Model model) {
+
+        Page<DiaChi> diaChi = diaChiService.getData(page);
+
+        model.addAttribute("hienthi", diaChi);
+
+        return "diachi";
+
+    }
+
+
 }
