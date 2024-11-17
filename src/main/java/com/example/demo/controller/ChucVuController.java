0ec7f22e0ddb92871dd31e2c4de2fc1378f20558
Duybv6703 <duybv6703@gmail.com>
2024-11-17 22:35:56 +0700

    create controller chucvu

---
+++ src/main/java/com/example/demo/controller/ChucVuController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.service.ChucVuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChucVuController {
+
+    @Autowired
+    private ChucVuService chucVuService;
+
+
+
+
+
+
+}
