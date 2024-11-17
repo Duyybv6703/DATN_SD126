83fb43bee43b82be47766e9eab78cf6484c3c53b
Duybv6703 <duybv6703@gmail.com>
2024-11-17 22:48:04 +0700

    create load data  controller chucvu

--- src/main/java/com/example/demo/controller/ChucVuController.java
+++ src/main/java/com/example/demo/controller/ChucVuController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.entity.ChucVu;
 import com.example.demo.service.ChucVuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChucVuController {
@@ -12,8 +17,17 @@ public class ChucVuController {
     private ChucVuService chucVuService;
 
 
+    @GetMapping("/chuc-vu")
+    String loadChucVu(@RequestParam(defaultValue = "0", name = "page") int page, Model model) {
+
+        Page<ChucVu> chucVu = chucVuService.getData(page);
+
+        model.addAttribute("hienthi", chucVu);
+
+        return "chucvu";
 
 
+    }
 
 
 }
