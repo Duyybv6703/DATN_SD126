c83a28267ce4a40cb5abce1a664d5ea030579594
Duybv6703 <duybv6703@gmail.com>
2024-11-20 22:19:18 +0700

    entity NhanVien

---
+++ src/main/java/com/example/demo/entity/NhanVien.java
@@ -0,0 +1,66 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "nhan_vien")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NhanVien {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "ma")
+    private String ma;
+
+    @Column(name = "ho_ten")
+    private String hoTen;
+
+    @Column(name = "sdt")
+    private String sdt;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "url_anh")
+    private String urlAnh;
+
+    @Column(name = "gioi_tinh")
+    private Boolean gioiTinh;
+
+    @Column(name = "ngay_sinh")
+    private String ngaySinh;
+
+    @Column(name = "que_quan")
+    private String queQuan;
+
+    @Column(name = "cccd")
+    private String cccd;
+
+    @Column(name = "tai_khoan")
+    private String taiKhoan;
+
+    @Column(name = "mat_khau")
+    private String matKhau;
+
+    @Column(name = "ngay_tao")
+    private String ngayTao;
+
+    @Column(name = "ngay_cap_nhat")
+    private String ngayCapNhat;
+
+    @Column(name = "tinh_trang")
+    private Boolean tinhTrang;
+
+
+}
