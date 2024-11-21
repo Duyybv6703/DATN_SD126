cc7902d9e923ed158730d38dbe6cd58f1f95c2ff
Duybv6703 <duybv6703@gmail.com>
2024-11-21 21:11:10 +0700

    add nhanvien controller

--- src/main/java/com/example/demo/controller/NhanVienController.java
+++ src/main/java/com/example/demo/controller/NhanVienController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -40,6 +41,44 @@ public class NhanVienController {
         model.addAttribute("detail", nhanVien);
 
         return "nhanvien";
+    }
+
+    @PostMapping("/nhan-vien/add")
+    public String add(
+            @RequestParam("ma") String ma1,
+            @RequestParam("hoten") String hoten1,
+            @RequestParam("sdt") String sdt1,
+            @RequestParam("email") String email1,
+            @RequestParam("urlanh") String urlanh1,
+            @RequestParam("gioitinh") String gioitinh1,
+            @RequestParam("ngaysinh") String ngaysinh1,
+            @RequestParam("quequan") String quequan1,
+            @RequestParam("cccd") String cccd,
+            @RequestParam("taikhoan") String taikhoan1,
+            @RequestParam("matkhau") String matkhau1,
+            @RequestParam("ngaytao") String ngaytao1,
+            @RequestParam("ngaycapnhat") String ngaycapnhat1,
+            @RequestParam("tinhtrang") String tinhtrang1
+    ) {
+        NhanVien nhanVien = NhanVien.builder()
+                .ma(ma1)
+                .hoTen(hoten1)
+                .sdt(sdt1)
+                .email(email1)
+                .urlAnh(urlanh1)
+                .gioiTinh(Boolean.valueOf(gioitinh1))
+                .ngaySinh(ngaysinh1)
+                .queQuan(quequan1)
+                .cccd(cccd)
+                .taiKhoan(taikhoan1)
+                .matKhau(matkhau1)
+                .ngayTao(ngaytao1)
+                .ngayCapNhat(ngaycapnhat1)
+                .tinhTrang(Boolean.valueOf(tinhtrang1))
+                .build();
+        nhanVienService.add(nhanVien);
+
+        return "redirect:/nhan-vien";
     }
 
 }
